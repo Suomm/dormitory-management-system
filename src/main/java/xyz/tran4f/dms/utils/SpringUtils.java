14e0cadac62133ea934d0b089f0b0b57d728b2b2
Suomm <1474983351@qq.com>
2020-12-23 21:48:54 +0800

    获取Spring Ioc容器中的对象工具类

--- src/main/java/xyz/tran4f/dms/utils/SpringUtils.java
+++ src/main/java/xyz/tran4f/dms/utils/SpringUtils.java
@@ -31,16 +31,8 @@ public class SpringUtils implements ApplicationContextAware {
         SpringUtils.applicationContext = applicationContext;
     }
 
-    public static Object getBean(String name){
-        return applicationContext.getBean(name);
-    }
-
     public static <T> T getBean(String name, Class<T> clazz){
         return applicationContext.getBean(name, clazz);
-    }
-
-    public static <T> T getBean(Class<T> clazz){
-        return applicationContext.getBean(clazz);
     }
 
 }
