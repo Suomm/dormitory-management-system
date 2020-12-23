ed7b4cc5708347fecb4fbe0b2d6f0c0e9fdda6d3
Suomm <1474983351@qq.com>
2020-12-23 21:49:45 +0800

    更新找回密码功能实现

--- src/main/java/xyz/tran4f/dms/controller/UserController.java
+++ src/main/java/xyz/tran4f/dms/controller/UserController.java
@@ -18,13 +18,12 @@ package xyz.tran4f.dms.controller;
 
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.ContextLoader;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
 import xyz.tran4f.dms.pojo.ResultInfo;
 import xyz.tran4f.dms.pojo.User;
@@ -32,7 +31,6 @@ import xyz.tran4f.dms.pojo.VerificationCode;
 import xyz.tran4f.dms.service.UserService;
 import xyz.tran4f.dms.utils.VerificationCodeUtils;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
@@ -65,42 +63,42 @@ public class UserController {
     }
 
     @PostMapping("register.html")
-    public String register(@Validated User user, String emailCode, BindingResult result, ModelMap map) {
-        map.addAttribute("user", user);
+    public String register(@Validated User user, String emailCode,
+                           BindingResult result, Model model,
+                           @SessionAttribute VerificationCode code) {
+        model.addAttribute("user", user);
         if (result.hasErrors()) {
             FieldError fieldError = result.getFieldError();
             assert fieldError != null;
             String message = fieldError.getDefaultMessage();
-            map.addAttribute("error", message);
+            model.addAttribute("error", message);
             return "user/register";
         }
-        VerificationCode code = (VerificationCode) map.getAttribute("code");
-        assert code != null;
         if (code.getOutDate() <= System.currentTimeMillis()) {
-            map.addAttribute("error", "验证码过期了");
+            model.addAttribute("error", "验证码过期了");
             return "user/register";
         }
         if (!code.getCode().equals(emailCode)) {
-            map.addAttribute("error", "验证码不对");
+            model.addAttribute("error", "验证码不对");
             return "user/register";
         }
         if (!"123456".equals(user.getValidateCode())) {
-            map.addAttribute("error", "邀请码不对哦");
+            model.addAttribute("error", "邀请码不对哦");
             return "user/register";
         }
         ResultInfo<User> info = userService.register(user);
         if (!info.isComplete()) {
-            map.addAttribute("error", info.getMessage());
+            model.addAttribute("error", info.getMessage());
             return "user/register";
         }
         return "redirect:/user/login.html";
     }
 
     @PostMapping("forget_password.html")
-    public String validateCode(User user, ModelMap map, HttpServletRequest request) {
+    public String validateCode(User user, Model model, HttpServletRequest request) {
         String digitalSignature = userService.digitalSignature(user);
         if (digitalSignature == null) {
-            map.addAttribute("error", "该用户没有注册");
+            model.addAttribute("error", "该用户没有注册");
             return "user/forget_password";
         }
         String path = request.getContextPath();
@@ -108,8 +106,8 @@ public class UserController {
         String resetPassHref = basePath+"user/reset_password?sid=" + digitalSignature+"&id="+user.getId();
         String emailContent = "请勿回复本邮件.点击下面的链接,重设密码<br/><a href="+resetPassHref +" target='_BLANK'>点击我重新设置密码</a>" +
                 "<br/>tips:本邮件超过10分钟,链接将会失效，需要重新申请'找回密码'\t"+digitalSignature;
-        userService.sendEmail(emailContent);
-        map.addAttribute("emailContent", emailContent);
+        userService.sendEmail(user.getId(), emailContent);
+        model.addAttribute("emailContent", emailContent);
         return "user/success";
     }
 
@@ -119,22 +117,22 @@ public class UserController {
     }
 
     @GetMapping("reset_password")
-    public String checkResetPassword(String sid, String id, ModelMap map) {
+    public String checkResetPassword(String sid, String id, Model model) {
         String s = userService.checkUser(sid, id);
         if (s != null) {
-            map.addAttribute("error", s);
+            model.addAttribute("error", s);
             return "redirect:/user/forget_password";
         }
         User user = new User();
         user.setId(id);
-        map.addAttribute("user", user);
+        model.addAttribute("user", user);
         return "user/reset_password";
     }
 
     @PostMapping("reset_password")
-    public String resetPassword(String password, ModelMap map) {
-        if (!userService.resetPassword((User) map.getAttribute("user"), password)) {
-            map.addAttribute("error", "重置密码失败");
+    public String resetPassword(String password, Model model) {
+        if (!userService.resetPassword((User) model.getAttribute("user"), password)) {
+            model.addAttribute("error", "重置密码失败");
             return "user/reset_password";
         }
         return "redirect:/user/login.html";
@@ -142,38 +140,11 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("pushVerificationCode")
-    public void pushVerificationCode(ModelMap map) {
+    public void pushVerificationCode(String id, Model model) {
         VerificationCode code = VerificationCodeUtils.getVerificationCode(6, 10 * 60 * 1000);
         String emailContent = "您的验证码为" + code.getCode() + "\t有效期为十分钟";
-        userService.sendEmail(emailContent);
-        map.addAttribute("code", code);
-        /*WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
-        assert context != null;
-        ServletContext servletContext = context.getServletContext();
-        assert servletContext != null;
-        servletContext.setAttribute("verificationCode", code);*/
-    }
-
-    /* ================================================================================================ */
-
-    @RequestMapping("/upload")
-    public String upload(MultipartFile file) throws IOException {
-        long startTime = System.currentTimeMillis();
-        System.out.println("fileName：" + file.getOriginalFilename());
-        String path = "D:/" + new Date().getTime() + file.getOriginalFilename();
-        File newFile = new File(path);
-        file.transferTo(newFile);
-        long endTime = System.currentTimeMillis();
-        System.out.println("运行时间：" + (endTime - startTime) + "ms");
-        return "index";
-    }
-
-    @RequestMapping("/testAjax")
-    @ResponseBody
-    public User testAjax(User user) {
-        System.out.println(user);
-        user.setUsername("王五");
-        return user;
+        userService.sendEmail(id, emailContent);
+        model.addAttribute("code", code);
     }
 
 }
