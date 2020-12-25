b5b05339b952d4bb79174d8aeff8e0553c4f4fb1
Suomm <1474983351@qq.com>
2020-12-25 23:37:46 +0800

    新增用户异常

---
+++ src/main/java/xyz/tran4f/dms/exception/EmailSendException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Wang Shuai (suomm.macher@foxmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.tran4f.dms.exception;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * <p>
+ * 邮件发送失败回显数据异常。
+ * </p>
+ *
+ * @author 王帅
+ * @since 1.0
+ */
+public class EmailSendException extends RuntimeMessageEchoSource {
+
+    private static final long serialVersionUID = -7074622825786549829L;
+
+    public EmailSendException(@NotNull String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/main/java/xyz/tran4f/dms/exception/RedirectException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 Wang Shuai (suomm.macher@foxmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.tran4f.dms.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 用于回显错误消息并重定向操作。
+ * </p>
+ *
+ * @author 王帅
+ * @since 1.0
+ */
+@Getter
+@Setter
+public class RedirectException extends RuntimeMessageEchoSource {
+
+    private static final long serialVersionUID = -36415506607708975L;
+
+    private String redirectUrl;
+
+    public RedirectException(String redirectUrl, MessageEchoSource cause) {
+        super(cause.getMessage(), cause, cause.getArgs());
+        setRedirectUrl(redirectUrl);
+    }
+
+}
