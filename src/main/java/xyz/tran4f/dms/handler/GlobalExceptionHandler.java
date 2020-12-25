878d1250df6902b9e20f151fb4afe7d0bf9e60bf
Suomm <1474983351@qq.com>
2020-12-25 23:39:38 +0800

    回显异常信息到前端

--- src/main/java/xyz/tran4f/dms/handler/GlobalExceptionHandler.java
+++ src/main/java/xyz/tran4f/dms/handler/GlobalExceptionHandler.java
@@ -16,11 +16,17 @@
 
 package xyz.tran4f.dms.handler;
 
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import xyz.tran4f.dms.attribute.WebAttribute;
+import xyz.tran4f.dms.exception.RuntimeMessageEchoSource;
+import xyz.tran4f.dms.exception.RedirectException;
+import xyz.tran4f.dms.utils.I18nUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -30,18 +36,53 @@ import javax.servlet.http.HttpServletRequest;
  * @author 王帅
  * @since 1.0
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Spring 依赖注入 I18nUtils 用于获取国际化异常信息
+
+    private final I18nUtils i18nUtils;
+
+    public GlobalExceptionHandler(I18nUtils i18nUtils) {
+        this.i18nUtils = i18nUtils;
+    }
+
+    @ExceptionHandler(RedirectException.class)
+    public String sendRedirect(HttpSession session, RedirectException e) {
+        session.setAttribute(WebAttribute.WEB_DEFAULT_ERROR, getMessage(e));
+        return "redirect:" + e.getRedirectUrl();
+    }
+
     /**
      * <p>
-     * 处理 {@link HttpRequestMethodNotSupportedException} 异常，将用户错误输入的 POST 请求
-     * 转换为 GET 请求，避免出现 {@code 405} 错误页面。
+     * 处理自定义异常中，将异常的详细信息回显到前端界面。
      * </p>
+     *
+     * @see RuntimeMessageEchoSource
+     * @see WebAttribute#WEB_DEFAULT_ERROR
      */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public String toOrigin(HttpServletRequest request) {
-        return "redirect:" + request.getServletPath() + ".html";
+    @ExceptionHandler(RuntimeMessageEchoSource.class)
+    public String sendMessage(HttpServletRequest request, RuntimeMessageEchoSource source) {
+        String message = getMessage(source);
+        // 将消息放入 request 域中用于回显
+        request.getSession().setAttribute(WebAttribute.WEB_DEFAULT_ERROR, message);
+        // 解析请求路径作为视图解析器返回
+        String result = request.getServletPath();
+        // 由于 GET/POST 请求都指向同一个 HTML 地址，所以要对路径做裁剪
+//        result = result.substring(1, result.length() - 5);
+        log.info("处理异常 {} 返回路径 {}", source.getClass().getName(), result);
+        return result;
+    }
+
+    private String getMessage(RuntimeMessageEchoSource source) {
+        try {
+            // 默认去本地查找 i18n 国际化消息
+            return i18nUtils.getMessage(source.getMessage(), source.getArgs());
+        } catch (NoSuchMessageException exception) {
+            // 本地没有匹配的国际化消息使用原始的消息
+            return source.getMessage();
+        }
     }
 
 }
