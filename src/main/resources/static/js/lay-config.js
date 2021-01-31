7394054f37371be4a19be63d351c7d4de4e44a18
Suomm <1474983351@qq.com>
2021-01-31 17:33:06 +0800

    前端资源文件

---
+++ src/main/resources/static/js/lay-config.js
@@ -0,0 +1,17 @@
+window.rootPath = (function (src) {
+    src = document.scripts[document.scripts.length - 1].src;
+    return src.substring(0, src.lastIndexOf("/") + 1);
+})();
+
+layui.config({
+    base: rootPath + "lay-module/",
+    version: true
+}).extend({
+    miniAdmin: "layuimini/miniAdmin", // layuimini后台扩展
+    miniMenu: "layuimini/miniMenu", // layuimini菜单扩展
+    miniPage: "layuimini/miniPage", // layuimini 单页扩展
+	miniTheme: "layuimini/miniTheme", // layuimini 主题扩展
+    step: 'step-lay/step', // 分步表单扩展
+    treetable: 'treetable-lay/treetable', //table树形扩展
+	template: 'template/template' //  Layui 数据表格模板
+});
